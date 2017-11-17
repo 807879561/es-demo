ed40b269317a0ded1f05efb3376d219b0adb27c2
shiyizhen <807879561@qq.com>
2017-11-17 16:50:38 +0800

    init

--- src/main/java/cn/syz/es/Test.java
+++ src/main/java/cn/syz/es/Test.java
@@ -1,5 +1,7 @@
 package cn.syz.es;
 
+import org.elasticsearch.action.delete.DeleteRequest;
+import org.elasticsearch.action.get.GetRequest;
 import org.elasticsearch.action.index.IndexRequestBuilder;
 import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.transport.TransportClient;
@@ -14,5 +16,12 @@ public class Test {
         TransportClient client=ElasticSearchClient.getClient();
 //        IndexRequestBuilder indexRequestBuilder = client.prepareIndex("website","blog").setSource("{\"title\":\"My second blog entry\",\"text\":\"Just trying this out...\",\"date\":\"2017/11/17\"}", XContentType.JSON);
 //        System.out.println(indexRequestBuilder.execute().actionGet());
+        System.out.println(client.connectedNodes());
+        System.out.println(client.filteredNodes());
+        System.out.println(client.listedNodes());
+        System.out.println(client.transportAddresses());
+        GetRequest getRequest=new GetRequest();
+        getRequest.index("");
+        System.out.println(client.get());
     }
 }
